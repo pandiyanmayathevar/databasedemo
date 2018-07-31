1bdb98149f3ba4e7c496ce0e0e8d4400c457e5ca
Pandiyan <pandiyan.mayathevar@gmail.com>
2018-07-31 16:52:24 -0400

    Initial Commit

---
+++ src/main/java/com/hpit/database/databasedemo/DatabasedemoApplication.java
@@ -0,0 +1,12 @@
+package com.hpit.database.databasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasedemoApplication.class, args);
+	}
+}
